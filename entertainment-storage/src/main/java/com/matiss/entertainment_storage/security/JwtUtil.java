b6538a42b480d58a974a6c81cd8ab7e6a751cb83
Matiss <matissciss@gmail.com>
2026-05-22 09:40:15 +0300

    updated JWT token

--- entertainment-storage/src/main/java/com/matiss/entertainment_storage/security/JwtUtil.java
+++ entertainment-storage/src/main/java/com/matiss/entertainment_storage/security/JwtUtil.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -11,11 +13,12 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private static final String SECRET = "MySuperSecretKeyThatIsAtLeast32CharactersLong!!";
+    @Value("${jwt.secret}")
+    private String secret;
     private static final long EXPIRATION_MS = 86400000;
 
     private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public String generateToken(String email) {
